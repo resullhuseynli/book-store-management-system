2541191bf005d2283a8ac3af8bb929676dc36fc1
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-07-11 14:31:41 +0400

    method deleted

--- src/main/java/com/store/book/dao/BookDAO.java
+++ src/main/java/com/store/book/dao/BookDAO.java
@@ -1,5 +1,6 @@
 package com.store.book.dao;
 
+import com.store.book.dao.entity.Author;
 import com.store.book.dao.entity.Book;
 import com.store.book.enums.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,5 @@ import java.util.List;
 public interface BookDAO extends JpaRepository<Book, Long> {
 
     List<Book> getBooksByGenre(Genre genre);
+    List<Book> getBooksByAuthor(Author author);
 }
