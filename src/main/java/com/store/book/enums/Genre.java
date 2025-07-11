1ef6e5626dbaf8071c9896d4dd205572c969159f
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-07-11 14:38:07 +0400

    added genres

--- src/main/java/com/store/book/enums/Genre.java
+++ src/main/java/com/store/book/enums/Genre.java
@@ -1,5 +1,19 @@
 package com.store.book.enums;
 
 public enum Genre {
-    FICTION
+    FICTION,
+    ROMANCE,
+    FANTASY,
+    MYSTERY,
+    HORROR,
+    BIOGRAPHY,
+    SELF_HELP,
+    HISTORY,
+    COOK_BOOKS,
+    TRAVEL,
+    AUTO_BIOGRAPHY,
+    THRILLER,
+    HISTORICAL_FICTION,
+    SCIENCE,
+    SCIENCE_FICTION
 }
