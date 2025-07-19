b3b727707b398083be73a2064681cd77f4605a0c
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-07-19 23:37:21 +0400

    mapper fixed

--- src/main/java/com/store/book/mapper/ItemMapper.java
+++ src/main/java/com/store/book/mapper/ItemMapper.java
@@ -1,7 +1,6 @@
 package com.store.book.mapper;
 
 import com.store.book.dao.CartRepository;
-import com.store.book.dao.dto.BookDtoResponse;
 import com.store.book.dao.dto.ItemDtoRequest;
 import com.store.book.dao.dto.ItemDtoResponse;
 import com.store.book.dao.entity.Book;
@@ -19,7 +18,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.math.BigDecimal;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Mapper(componentModel = "spring")
@@ -36,53 +34,36 @@ public abstract class ItemMapper {
 
     @Mappings({
             @Mapping(target = "book", source = "request", qualifiedByName = "getBook"),
-            @Mapping(target = "itemPrice", source = "request", qualifiedByName = "getPrice"),
-            @Mapping(target = "itemTotalPrice", source = "request", qualifiedByName = "getTotalPrice"),
             @Mapping(target = "cart", source = "request", qualifiedByName = "getCart")
     })
     public abstract Item dtoToEntity(ItemDtoRequest request);
 
     @Mappings({
             @Mapping(target = "bookName", source = "book.title"),
-            @Mapping(target = "price", source = "itemPrice"),
-            @Mapping(target = "totalPrice", source = "itemTotalPrice")
+            @Mapping(target = "price", source = "book.newPrice"),
+            @Mapping(target = "totalPrice", source = "item", qualifiedByName = "getItemTotalPrice")
     })
     public abstract ItemDtoResponse entityToDto(Item item);
+
+    @Named("getItemTotalPrice")
+    BigDecimal getItemTotalPrice(Item item) {
+        Book book = item.getBook();
+        return book.getNewPrice().multiply(BigDecimal.valueOf(item.getQuantity()));
+    }
 
     @Named("getBook")
     Book getBook(ItemDtoRequest request) {
         return bookService.getBookWithDetailsById(request.getBookId());
     }
 
-    @Named("getPrice")
-    BigDecimal getPrice(ItemDtoRequest request) {
-        Book book = getBook(request);
-        BookDtoResponse response = bookMapper.entityToDto(book);
-        return response.getNewPrice();
-    }
-
-    @Named("getTotalPrice")
-    BigDecimal getTotalPrice(ItemDtoRequest request) {
-        return getPrice(request).multiply(BigDecimal.valueOf(request.getQuantity()));
-    }
-
     @Named("getCart")
     Cart getCart(ItemDtoRequest request) {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         UserEntity user = customUserDetailsService.loadUserByUsername(username);
-        Optional<Cart> cart = cartRepository.findByUser(user);
-        if (cart.isPresent()) {
-            if (cart.get().getTotalPrice() == null) {
-                cart.get().setTotalPrice(BigDecimal.ZERO);
-            }
-            cart.get().setTotalPrice(cart.get().getTotalPrice().add(getTotalPrice(request)));
-            cartRepository.save(cart.get());
-            return cart.get();
-        }
-        Cart newCart = new Cart();
-        newCart.setUser(user);
-        newCart.setTotalPrice(getTotalPrice(request));
-        cartRepository.save(newCart);
-        return newCart;
+        return cartRepository.findByUser(user).orElseGet(() -> {
+            Cart newCart = new Cart();
+            newCart.setUser(user);
+            return cartRepository.save(newCart);
+        });
     }
 }
