9cd690c8aece6e752320d56023955c13ba5fa0ce
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-07-19 23:39:31 +0400

    get author by name fixed with LIKE

--- src/main/java/com/store/book/dao/AuthorRepository.java
+++ src/main/java/com/store/book/dao/AuthorRepository.java
@@ -2,6 +2,7 @@ package com.store.book.dao;
 
 import com.store.book.dao.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
+    @Query("SELECT a FROM Author a WHERE LOWER(a.name) LIKE LOWER(CONCAT('%', :name, '%'))")
     List<Author> getAuthorsByName(String name);
     boolean existsAuthorsByName(String username);
 
