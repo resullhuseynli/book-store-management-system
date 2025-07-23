8240d5119bcf4a36f4c943cee86a275ab59445b7
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-07-23 22:45:39 +0400

    reformat

--- src/main/java/com/store/book/exception/GlobalExceptionHandler.java
+++ src/main/java/com/store/book/exception/GlobalExceptionHandler.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.io.IOException;
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static org.springframework.http.ResponseEntity.badRequest;
 
