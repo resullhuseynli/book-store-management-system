eb98fe8ee11d3fc4c1b6f90994f85d9183625c0e
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-07-24 16:05:25 +0400

    the request header of Accept-Language added

--- src/main/java/com/store/book/configs/OpenApiConfig.java
+++ src/main/java/com/store/book/configs/OpenApiConfig.java
@@ -2,8 +2,12 @@ package com.store.book.configs;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.HeaderParameter;
+import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,5 +26,27 @@ public class OpenApiConfig {
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
                                         .bearerFormat("JWT")));
+    }
+
+    @Bean
+    public OpenApiCustomizer addAcceptLanguageHeader() {
+        return openApi -> {
+            HeaderParameter acceptLanguageHeader = (HeaderParameter) new HeaderParameter()
+                    .name("Accept-Language")
+                    .description("Language for localization")
+                    .example("az")
+                    .required(false)
+                    .schema(new StringSchema());
+
+            if (openApi.getComponents() == null) {
+                openApi.setComponents(new Components());
+            }
+
+            openApi.getPaths().values().forEach(pathItem -> {
+                pathItem.readOperations().forEach(operation -> {
+                    operation.addParametersItem(acceptLanguageHeader);
+                });
+            });
+        };
     }
 }
